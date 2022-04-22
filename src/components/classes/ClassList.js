f8f519bb8524f863f6a0d131aef0c279718be193
LukeSlater02 <lukeslater101@gmail.com>
2022-04-22 16:19:46 -0500

    started up on the input forms

--- src/components/classes/ClassList.js
+++ src/components/classes/ClassList.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { getAllClasses } from "../../modules/ClassDataManager";
 import { ClassCard } from "./ClassCard";
+import { useNavigate } from "react-router-dom";
+import { addSheet } from "../../modules/SheetDataManager";
 
 export const ClassList = () => {
     const [classes, setClasses] = useState([])
@@ -11,13 +13,13 @@ export const ClassList = () => {
 
     return (
         <>
-       <h2>Choose a Class to Begin</h2>
-        <div className="classCardsList">
-            {/* NEED TO ADD SELECT CLASS BUTTON FUNCTION */}
-            {classes.map(c => {
-                return <ClassCard key={c.id} classObj={c} />
-            })}
-        </div>
+            <h2>Choose a Class to Begin</h2>
+            <div className="classCardsList">
+                {/* NEED TO ADD SELECT CLASS BUTTON FUNCTION */}
+                {classes.map(c => {
+                    return <ClassCard key={c.id} classObj={c}/>
+                })}
+            </div>
         </>
     )
 }
--- src/modules/ClassDataManager.js
+++ src/modules/ClassDataManager.js
@@ -8,3 +8,7 @@ const data = 'http://localhost:8088'
 export const getAllClasses = () => {
     return fetch(`${data}/classes`).then(res => res.json())
 }
+
+export const getClassById = id => {
+    return fetch(`${data}/classes/${id}`).then(res => res.json())
+}
