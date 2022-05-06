b1fe5d14bb25522205118dd1b544b6bc78373d61
LukeSlater02 <lukeslater101@gmail.com>
2022-05-06 13:46:23 -0500

    started working on spell translate

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import './index.css';
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { DnDMain } from './components/Dndmain';
-import { Footer } from './components/foot/Footer';
-import { NavBar } from './components/nav/NavBar';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -13,7 +11,6 @@ root.render(
   <>
     <Router>
       <DnDMain />
-      <Footer />
     </Router>
   </>
 );
